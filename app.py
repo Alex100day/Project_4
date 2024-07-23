70b21a6a71e146628ed0a8cf6d818713dd3ace4f
Iskandar <alexkh2017@gmail.com>
2024-07-23 01:30:39 -0500

    app.py has been updated

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ import plotly.express as px
 vehicles = pd.read_csv('vehicles_us.csv')
 
 st.header('Web application development')
+introduction = """ In this project we will develop and deploy a web application to a cloud service making it accessible to the public. \ 
+ The projects tasks include opening accounts on github and render.com. Git repositary with README.md and gitignore files will be created. \ 
+ Jupyter notebook platform, VS Code (as an IDE) and python with its packages necessary to perform the project taks will be used.  \ 
+ We will work on the dataset on car sales advertisements. First, using Jupyter notebook in VS Code IDE the dataset will be preprocced \ 
+ (working on missing and duplicated values). Further analisys will be done by excluding outlires and finding the correlations between column values. \ 
+ The revealed correlations we will visualized by plotting (scatter and histogram) the figures. These results will be stored in EDA.ipynb.
+Next, in order to develop web applications we create app.py file in the root directory of the project. The main results including the obtained \ 
+figures in EDA.ippynb will be copied to app.py. After getting all files with necessary results and contents we will commit and push them \ 
+to the git repository. Committing and pushing processes of any update/change on the git repository files and directories will be done repeatedly \ 
+until getting the final results on the web app. """
 st.subheader('Car advertisement')
 
 
@@ -58,5 +68,8 @@ if st.checkbox('Toggle Plot Type'):
 st.markdown('## Main outcomes.')
 outcome = """ The analysis of the dataset after preprocessing, grouping and filtering allows obtaining correlation \
     between car prices and model year and car conditions. These correlations visualized by histograms and scatter-style figures. \
-    The git repositary contains all updated results and files for sebsequnet deployment on render.com """
+    The above shown figure demonstrates the gradual increase of avarage prices of cars from older to newer ones. \
+    The above shown histogram demonstrates that the cars produced 2011 - 2013 are most popular ones. \ 
+    The decrease corresponding to 2009 - 2010 can be caused by the crisis in economy. 
+    """
 st.markdown(outcome)
