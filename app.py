77b8a7f55c76e7432d8e168453c68e1f94b4b84d
Iskandar <alexkh2017@gmail.com>
2024-07-23 02:01:06 -0500

    app.py has been updated

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ vehicles = pd.read_csv('vehicles_us.csv')
 
 st.header('Web application development')
 introduction = """
-    In this project we will develop and deploy a web application to a cloud service making it accessable to the public. \ 
-    The projects tasks include opening accounts on github and render.com. Git repositary with README.md and gitignore files will be created. \ 
-    Jupyter notebook platform, VS Code (as an IDE) and python with its packages necessary to perform the project taks will be used.  \ 
-    We will work on the dataset on car sales advertisements. First, using Jupyter notebook in VS Code IDE the dataset will be preprocced \ 
-    (working on missing and duplicated values). Further analisys will be done by excluding outlires and finding the correlations between column values. \ 
-    The revealed correlations we will visualized by plotting (scatter and histogram) the figures. These results will be stored in EDA.ipynb.
-    Next, in order to develop web applications we create app.py file in the root directory of the project. The main results including the obtained \ 
-    figures in EDA.ippynb will be copied to app.py. After getting all files with necessary results and contents we will commit and push them \ 
-    to the git repository. Committing and pushing processes of any update/change on the git repository files and directories will be done repeatedly \ 
+    In this project we will develop and deploy a web application to a cloud service making it accessable to the public. The projects 
+    tasks include opening accounts on github and render.com. Git repositary with README.md and gitignore files will be created. 
+    Jupyter notebook platform, VS Code (as an IDE) and python with its packages necessary to perform the project taks will be used. 
+    We will work on the dataset on car sales advertisements. First, using Jupyter notebook in VS Code IDE the dataset will be preprocced 
+    (working on missing and duplicated values). Further analisys will be done by excluding outlires and finding the correlations between column values. 
+    The revealed correlations we will visualized by plotting (scatter and histogram) the figures. These results will be stored in EDA.ipynb. 
+    Next, in order to develop web applications we create app.py file in the root directory of the project. The main results including the obtained 
+    figures in EDA.ippynb will be copied to app.py. After getting all files with necessary results and contents we will commit and push them 
+    to the git repository. Committing and pushing processes of any update/change on the git repository files and directories will be done repeatedly  
     until getting the final results on the web app. """
-st.subheader(introduction)
+st.markdown(introduction)
 
 
 #Preprosessing ['model_year'], ['cylinders'], ['odometer'] columns
@@ -70,7 +70,7 @@ st.markdown('#Main outcomes.')
 outcome = """ The analysis of the dataset after preprocessing, grouping and filtering allows obtaining correlation \
     between car prices and model year and car conditions. These correlations visualized by histograms and scatter-style figures. \
     The above shown figure demonstrates the gradual increase of avarage prices of cars from older to newer ones. \
-    The above shown histogram demonstrates that the cars produced 2011 - 2013 are most popular ones. \ 
+    The above shown histogram demonstrates that the cars produced 2011 - 2013 are most popular ones.  
     The decrease corresponding to 2009 - 2010 can be caused by the crisis in economy. 
     """
 st.markdown(outcome)
