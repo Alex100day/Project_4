5674b2e0f788d1cfb7323929976f5c75b3a9a7a9
Iskandar <alexkh2017@gmail.com>
2024-07-23 01:02:19 -0500

    Updated app.py and EDA.ipynb

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import plotly.express as px
 
 vehicles = pd.read_csv('vehicles_us.csv')
 
-st.header('Vehicles_adv')
-st.subheader('Car_price')
+st.header('Web application development')
+st.subheader('Car advertisement')
+
 
 #Histogram
 
@@ -16,13 +17,13 @@ hist_model_year = px.bar(model_year_order, x='model_year', y='count',
 hist_model_year.update_traces(marker=dict(line=dict(width=2, color='Blue')))
 hist_model_year.show()
 
-st.write("### Histogram model year")
+st.write("###Histogram model year")
 st.plotly_chart(hist_model_year)
 
 
 # Scatter plot
 
-fig_model_year = px.scatter(model_year_order, x='model_year', y='mean', title='Model year vs price')
+fig_model_year = px.scatter(model_year_order, x='model_year', y='mean', title='model year vs price')
 fig_model_year.update_layout(xaxis_title='Model year', yaxis_title='Average price')
 fig_model_year.show()
 
@@ -38,3 +39,10 @@ if st.checkbox('Toggle Plot Type'):
     elif plot_type == 'Scatter Plot':
         st.plotly_chart(fig_model_year)
 
+
+
+st.markdown('## Main outcomes.')
+outcome = """ The analysis of the dataset after preprocessing, grouping and filtering allows obtaining correlation \
+    between car prices and model year and car conditions. These correlations visualized by histograms and scatter-style figures. \
+    The git repositary contains all updated results and files for sebsequnet deployment on render.com """
+st.markdown(outcome)
