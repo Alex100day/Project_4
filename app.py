496c58c19e8c8a06909aee338367f5f88bd48695
Iskandar <alexkh2017@gmail.com>
2024-07-23 01:18:35 -0500

    app.py and EDA.ipynb have been updated

--- app.py
+++ app.py
@@ -9,6 +9,20 @@ st.header('Web application development')
 st.subheader('Car advertisement')
 
 
+#Preprosessing ['model_year'], ['cylinders'], ['odometer'] columns
+
+vehicles['model_year'] = vehicles.groupby('model')['model_year'].transform(lambda x: x.fillna(x.median()))
+vehicles['cylinders'] = vehicles.groupby('model')['cylinders'].transform(lambda x: x.fillna(x.median()))
+vehicles['odometer'] = vehicles.groupby('model_year')['odometer'].transform(lambda x: x.fillna(x.median()))
+
+
+# 'model_year' is strongly right-skewed, and consider only cars produced since 1995, excluding older cars as outlires 
+
+lower_year = 1995
+vehicles = vehicles[vehicles['model_year'] >= lower_year]
+
+# Here, we study the dependence of the car prices on their model years
+
 #Histogram
 
 model_year_order = vehicles.groupby('model_year')['price'].agg(['mean', 'count']).reset_index()
